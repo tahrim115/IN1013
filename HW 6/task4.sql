6fd5fee1c17a21699768aefb7a087acb63e42523
tahrim115 <tahrim.rahman@city.ac.uk>
2024-12-01 16:18:56 +0000

    Update task4.sql

--- HW 6/task4.sql
+++ HW 6/task4.sql
@@ -1,28 +1,67 @@
 -- 1
 
-SELECT SUM(bill_total) AS Income
-FROM restBill
+SELECT DISTINCT rb.cust_name
+FROM restBill rb
+WHERE rb.bill_total > 450.00
+  AND rb.waiter_no IN (
+    SELECT rs.staff_no
+    FROM restStaff rs
+    WHERE rs.headwaiter = (
+        SELECT staff_no
+        FROM restStaff
+        WHERE first_name = 'Charles'
+    )
+);
 
 -- 2
 
-SELECT SUM(bill_total)
-FROM restBill
-WHERE bill_date LIKE "1602__";
+SELECT DISTINCT rS.first_name, rS.surname
+FROM restRoom_management rM
+JOIN restStaff rS ON rM.headwaiter = rS.staff_no
+WHERE rM.room_name = (
+    SELECT rT.room_name
+    FROM restRest_table rT
+    JOIN restBill rB ON rT.table_no = rB.table_no
+    WHERE rB.cust_name = 'Nerida Smith' AND rB.bill_date = 160111
+);
 
 -- 3
 
-SELECT AVG(bill_total)
+SELECT cust_name
 FROM restBill
-WHERE table_no = "2";
+WHERE bill_total = (
+    SELECT MIN(bill_total)
+    FROM restBill
+);
 
 -- 4
 
-SELECT MIN(no_of_seats), MAX(no_of_seats), AVG(no_of_seats)
-FROM restRest_table
-WHERE room_name = "Blue";
+SELECT first_name, surname
+FROM restStaff
+WHERE staff_no NOT IN (
+    SELECT DISTINCT waiter_no
+    FROM restBill
+);
 
 -- 5
 
+SELECT DISTINCT s.first_name, s.surname
+FROM restStaff s
+JOIN restBill b ON s.staff_no = b.waiter_no
+WHERE b.table_no IN (
 SELECT DISTINCT table_no
 FROM restBill
-WHERE waiter_no IN (002, 004);
+WHERE bill_no IN (00014, 00017)
+);
+
+-- 6
+
+SELECT rb.cust_name, rs.first_name AS headwaiter_first_name, rs.surname AS headwaiter_surname, rt.room_name
+FROM restBill rb
+JOIN restRest_table rt ON rb.table_no = rt.table_no
+JOIN restRoom_management rm ON rt.room_name = rm.room_name AND rb.bill_date = rm.room_date
+JOIN restStaff rs ON rm.headwaiter = rs.staff_no
+WHERE rb.bill_total = (
+    SELECT MAX(bill_total)
+    FROM restBill
+);
