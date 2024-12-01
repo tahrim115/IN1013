f8c97382f12559688410870a69357d65190e35e5
tahrim115 <tahrim.rahman@city.ac.uk>
2024-12-01 16:33:26 +0000

    Update task5.sql

--- HW 6/task5.sql
+++ HW 6/task5.sql
@@ -1,37 +1,48 @@
 -- 1
 
-SELECT bill_total || ", " || bill_date
-FROM restBill
-WHERE cust_name = "Bob Crow";
+SELECT rs.first_name, rs.surname, rb.bill_date, COUNT(rb.bill_no) AS num_bills
+FROM restBill rb
+JOIN restStaff rs ON rb.waiter_no = rs.staff_no
+GROUP BY rs.staff_no, rb.bill_date
+HAVING COUNT(rb.bill_no) >= 2;
 
 -- 2
 
-SELECT DISTINCT cust_name
-FROM restBill
-WHERE cust_name LIKE "%Smith"
-ORDER BY cust_name DESC;
+SELECT rm.room_name, COUNT(rt.table_no) AS num_large_tables
+FROM restRest_table rt
+JOIN restRoom_management rm ON rt.room_name = rm.room_name
+WHERE rt.no_of_seats > 6
+GROUP BY rm.room_name;
 
 -- 3
 
-SELECT DISTINCT cust_name
-FROM restBill
-WHERE cust_name LIKE "% C%";
+SELECT rm.room_name, SUM(rb.bill_total) AS total_bills
+FROM restBill rb
+JOIN restRest_table rt ON rb.table_no = rt.table_no
+JOIN restRoom_management rm ON rt.room_name = rm.room_name
+GROUP BY rm.room_name;
 
 -- 4
 
-SELECT first_name || " " || surname
-FROM restStaff
-WHERE headwaiter IS NOT NULL;
+SELECT rs.first_name AS headwaiter_first_name, rs.surname AS headwaiter_surname, SUM(rb.bill_total) AS total_bill_amount
+FROM restRoom_management rm
+JOIN restStaff rs ON rm.headwaiter = rs.staff_no
+JOIN restRest_table rt ON rm.room_name = rt.room_name
+JOIN restBill rb ON rt.table_no = rb.table_no
+GROUP BY rs.staff_no
+ORDER BY total_bill_amount DESC;
 
 -- 5
 
-SELECT *
-FROM restBill
-WHERE bill_date LIKE "1602__";
+SELECT b.cust_name, AVG(b.bill_total) AS avg_spent
+FROM restBill b
+GROUP BY b.cust_name
+HAVING AVG(b.bill_total) > 400;
 
 -- 6
 
-SELECT DISTINCT bill_date
-FROM restBill
-WHERE bill_date LIKE "15____"
-ORDER BY bill_date ASC;
+SELECT rs.first_name, rs.surname, COUNT(rb.bill_no) AS num_bills
+FROM restBill rb
+JOIN restStaff rs ON rb.waiter_no = rs.staff_no
+GROUP BY rs.staff_no, rb.bill_date
+HAVING COUNT(rb.bill_no) >= 3;
